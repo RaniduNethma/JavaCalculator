1b16b9912f6a6d43f39fa28f8a88f510de18ecbb
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2024-11-30 20:44:10 +0530

    update-java-Calculator
    update
    1. infinity loop
    2. divide by zero feature

--- src/JavaCalculator.java
+++ src/JavaCalculator.java
@@ -3,52 +3,65 @@ import java.util.Scanner;
 public class JavaCalculator {
     public static void main(String[] args){
 
-        char operator;
+        String operator;
         double numb1, numb2, result = 0;
 
-        Scanner input = new Scanner(System.in);
+        while (true){
 
-        System.out.print("Choose an operator: +, -, *, /, ^, or % : ");
-        operator = input.next().charAt(0);
+            Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter first number : ");
-        numb1 = input.nextDouble();
+            System.out.print("Choose an operator: +, -, *, /, ^, or % : ");
+            operator = input.nextLine();
 
-        System.out.print("Enter second number : ");
-        numb2 = input.nextDouble();
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") ||
+                    operator.equals("/") || operator.equals("^") || operator.equals("%")){
 
-        switch (operator){
+                System.out.print("Enter first number : ");
+                numb1 = input.nextDouble();
 
-            case '+':
-                result = numb1 + numb2;
-                break;
+                System.out.print("Enter second number : ");
+                numb2 = input.nextDouble();
 
-            case '-':
-                result = numb1 - numb2;
-                break;
+                switch (operator){
 
-            case '*':
-                result = numb1 * numb2;
-                break;
+                    case "+":
+                        result = numb1 + numb2;
+                        break;
 
-            case '/':
-                result = numb1 / numb2;
-                break;
+                    case "-":
+                        result = numb1 - numb2;
+                        break;
 
-            case '^':
-                result = Math.pow(numb1, numb2);
-                break;
+                    case "*":
+                        result = numb1 * numb2;
+                        break;
 
-            case '%':
-                result = numb1 % numb2;
-                break;
+                    case "/":
+                        if (numb2 != 0){
+                            result = numb1 / numb2;
+                        }
+                        else {
+                            System.out.println("Cannot divide by zero");
+                        }
+                        break;
 
-            default:
-                System.out.println("Invalid operator!");
-                break;
+                    case "^":
+                        result = Math.pow(numb1, numb2);
+                        break;
+
+                    case "%":
+                        result = numb1 % numb2;
+                        break;
+
+                    default:
+                        System.out.println("Invalid operator!");
+                        break;
+                }
+
+                if (numb2 != 0){
+                    System.out.println(numb1 + " " + operator + " " + numb2 + " = " + result + "\n");
+                }
+            }
         }
-
-        System.out.println(numb1 + " " + operator + " " + numb2 + " = " + result);
-        input.close();
     }
 }
